bf18ec9eb2b34404da5cf30c452bd84c8d488b0f
unknown <m_bogdanova621@mail.ru>
2022-10-22 21:16:24 +0400

    получение новых откликов

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import mongoose from "mongoose";
+import FeedbackModel from "./db.js";
 
 mongoose.connect("mongodb://localhost:27017/feedback", {
   useNewUrlParser: true,
@@ -8,6 +9,34 @@ mongoose.connect("mongodb://localhost:27017/feedback", {
 });
 
 const app = express();
+
+async function getNewFeedbacks() {
+  const { id, date } = await FeedbackModel.findOne().sort({ date: -1 });
+  let lastFeedBack;
+  const newFeedbacks = [];
+  for (let i = 0; ; i++) {
+    const {
+      data: { reviews },
+    } = await axios(
+      `https://api.delivery-club.ru/api1.2/reviews?chainId=48274&limit=10&offset=${i}0`
+    );
+    lastFeedBack = reviews.find(({ orderHash }) => orderHash === id);
+    newFeedbacks.push(
+      ...reviews
+        .filter(({ rated }) => new Date(rated) > date)
+        .map(({ answers, author, body, icon, rated, orderHash }) => ({
+          answer: answers[0]?.answer?.trim() || "",
+          author: author || "incognito",
+          text: body.trim() || "",
+          icon,
+          date: new Date(rated),
+          id: orderHash,
+        }))
+    );
+    if (lastFeedBack) break;
+  }
+}
+getNewFeedbacks();
 
 // app.get("/", async(req, res) => {
 // const info = await parse();
